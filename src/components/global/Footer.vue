1e2473cf2e4760b15de045431d5594fffadde431
JonasChen <tom200425@gamil.com>
2022-03-25 13:10:37 +0800

    修正 - 版本號動態導入

--- src/components/global/Footer.vue
+++ src/components/global/Footer.vue
@@ -7,8 +7,22 @@
             <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2022 partygame —
                 <a href="#" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@Jonas</a>
             </p>
-            <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">版本 1.0.0</span>
+            <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">版本 {{this.version}}</span>
         </div>
     </footer>
 
 </template>
+
+<script>
+export default {
+    name:'Footer',
+    data(){
+        return{
+            version:process.env.PACKAGE_VERSION
+        }
+    },
+    mounted(){
+        console.log(process.env);
+    }
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.plugin("define").tap(definitions => {
+      definitions[0]["process.env"]["PACKAGE_VERSION"] = JSON.stringify(
+        require("./package.json").version,
+      );
+      return definitions;
+    });
+  },
 })
